e8af491312b5ca018b5d59088539a3cda8dca617
dengkaix <shicpblmlb@gmail.com>
2022-04-30 16:59:06 +0800

    first commit

--- r.py
+++ r.py
@@ -11,3 +11,9 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('平均留言長度', sum_len/len(data))
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '留言長度小於100')
+print(new[0])
